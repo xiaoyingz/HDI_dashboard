42d95e544bc55d2ac74e0300e5b4e34f0be575b6
jiaxinying <jiaxinyi@umich.edu>
2021-10-18 22:23:52 -0400

    add api for getting movie

--- database_mysql.py
+++ database_mysql.py
@@ -1,25 +1,32 @@
 import mysql.connector
 from collections import defaultdict
 
+
 def get_movies(country=None, year=None, avg_vote=None, genre=None):
     """
     :return: a list of dictionary with key as field name
     """
     cnx = mysql.connector.connect(user='root', database='mp')
     cursor = cnx.cursor()
-    query = ("SELECT * FROM movie "
-         "WHERE country = %s")
-    params = [country]
+    query = "SELECT * FROM movie"
+    conditions = []
+    params = []
+    if country is not None:
+        conditions.append("country = %s")
+        params.append(country)
     if year is not None:
-        query += " AND year = %s"
+        conditions.append("year = %s")
         params.append(year)
     if genre is not None:
-        query += " AND genre = %s"
+        conditions.append("genre = %s")
         params.append(genre)
     if avg_vote is not None:
+        conditions.append("avg_vote >= %s AND avg_vote <= %s")
         start, end = avg_vote
-        query += " AND avg_vote >= %s AND avg_vote <= %s"
         params.extend([start, end])
+    if conditions:
+        query += " WHERE "
+        query += " AND ".join(conditions)
     cursor.execute(query, tuple(params))
     fields = [i[0] for i in cursor.description]
     res = []
@@ -29,5 +36,6 @@ def get_movies(country=None, year=None, avg_vote=None, genre=None):
     cnx.close()
     return res
 
-movies = get_movies('China', year=2010, genre="Drama", avg_vote=(7,9))
-print(movies)
+if __name__ == '__main__':
+    movies = get_movies(country='China', year=2010, genre="Drama", avg_vote=(6, 8))
+    print(movies)
--- server.py
+++ server.py
@@ -4,10 +4,12 @@ import json
 from flask import Flask, request, abort, jsonify, make_response
 from flask_cors import CORS, cross_origin
 import database_mongo
+import database_mysql
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/')
 def welcome():
@@ -16,6 +18,7 @@ def welcome():
     :return: welcome message
     """
     return "HDI_dashboard"
+
 
 @app.route(rule='/ratings', methods=['GET', 'PUT'])
 def get_or_put_rating_vote():
@@ -38,18 +41,38 @@ def get_or_put_rating_vote():
         print(items)
         for item in items:
             key, var = item
-            if key not in database_mongo.DOUBLE_ATTRI and key not in database_mongo.INT_ATTRI:\
-                abort(400, "Invalid attribute: " + key)
+            if key not in database_mongo.DOUBLE_ATTRI and key not in database_mongo.INT_ATTRI: \
+                    abort(400, "Invalid attribute: " + key)
 
         status = database_mongo.update_data(title_id, new_data, "Ratings")
         if status == 0:
-            return jsonify("rating with imdb_title_id: "+title_id+" has been updated."), 201
+            return jsonify("rating with imdb_title_id: " + title_id + " has been updated."), 201
         elif status == 1:
             abort(400, "No ratings with such title_id.")
         elif status == 2:
             abort(400, "Unmatched field.")
         else:
             abort(400, "Invalid type of newValue.")
+
+
+@app.route(rule='/movies', methods=['GET'])
+def get_movies_by_conditions():
+    """
+    GET: /ratings?country={country}&year={year}&avg_vote={avg_vote}&genre={genre}
+
+    :return: status code and message
+    """
+    country = request.args.get('country', None)
+    year = request.args.get('year', None)
+    if year is not None:
+        year = int(year)
+    avg_vote = request.args.get('avg_vote', None)
+    if avg_vote is not None:
+        avg_vote = tuple([int(item) for item in avg_vote.split(',')])
+    genre = request.args.get("genre", None)
+    result = database_mysql.get_movies(country=country, year=year, avg_vote=avg_vote, genre=genre)
+    return jsonify(result), 200
+
 
 def check_content_type(request):
     """
@@ -60,6 +83,7 @@ def check_content_type(request):
     """
     if request.content_type != 'application/json':
         abort(415)
+
 
 def check_id(curr_request):
     """
@@ -73,9 +97,11 @@ def check_id(curr_request):
         abort(400, "Please specify id.")
     return curr_id
 
+
 def check_if_found(result):
     if len(result) == 0:
         abort(404, "Record not found")
+
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -90,6 +116,7 @@ def not_found(error):
 @app.errorhandler(415)
 def invalid_content_type(error):
     return make_response(jsonify({'message': "Data must be JSON."}), 415)
+
 
 if __name__ == '__main__':
     app.run()
